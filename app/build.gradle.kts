edf8946faa5e7b69b037a7cfd1c8c65cfd6e2f3f
Yuki <yukiseno0911@gmail.com>
2026-09-17 21:58:02 +0900

    v1.1.1: フルデバッグ修正のリリース

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "jp.yuki.a6000transfer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
     }
 
     buildTypes {
